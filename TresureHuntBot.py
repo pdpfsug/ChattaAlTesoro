ecf14b113ee983eb718ebc86078602ad9ae8ad78
Radeox <dawid.weglarz95@gmail.com>
2017-09-27 22:32:36 +0200

    Team registration

--- TresureHuntBot.py
+++ TresureHuntBot.py
@@ -9,6 +9,7 @@ import os
 import sys
 import uuid
 import gettext
+import sqlite3
 
 from time import sleep
 from datetime import datetime
@@ -23,7 +24,6 @@ def handle(msg):
     """
     This function handle all incoming messages from users
     """
-
     content_type, chat_type, chat_id = telepot.glance(msg)
     chat_id = msg['chat']['id']
 
@@ -33,9 +33,17 @@ def handle(msg):
         if command_input == '/start':
             bot.sendMessage(chat_id, _('start_msg'))
 
-        if command_input == '/stop':
-            # bot.sendMessage(chat_id, '{0}')
-            pass
+        elif command_input == '/registerTeam':
+            user_state[chat_id] = 1
+            bot.sendMessage(chat_id, _('team_name_msg'))
+        
+        elif user_state[chat_id] == 1:
+            lname = msg['from']['first_name'] + ' ' + msg['from']['last_name']
+
+            if add_team(chat_id, command_input, lname):
+                bot.sendMessage(chat_id, _('registration_success'))
+            else:
+                bot.sendMessage(chat_id, _('registration_fail'))
 
     elif content_type == 'photo':
         msg = msg['photo'][-1]['file_id']
@@ -64,6 +72,31 @@ def handle(msg):
             os.remove(filename)
 
 
+def add_team(chat_id, team_name, leader_name):
+    """
+    Add new Team to database
+    """
+
+    # Open DB
+    conn = sqlite3.connect('treasure_hunt.db')
+    c = conn.cursor()
+
+    try:
+        c.execute('''INSERT INTO team VALUES({0}, '{1}', '{2}')'''.format(chat_id,
+                                                                          team_name,
+                                                                          leader_name))
+        conn.commit()
+
+        rv = 1
+    except:
+        rv = 0
+    finally:
+        # Finally close connection
+        conn.close()
+
+        return rv
+            
+
 def log_print(text):
     """
     Logging functon with date/time
@@ -81,6 +114,9 @@ def log_print(text):
 
 # Main
 print("Starting TreasureHuntBot...")
+
+# Variables
+user_state = {}
 
 # PID file
 pid = str(os.getpid())
