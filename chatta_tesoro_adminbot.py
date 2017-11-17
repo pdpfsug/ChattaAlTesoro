65116a70a18e76b22719f4c63b0c61434988e53d
Radeox <dawid.weglarz95@gmail.com>
2017-11-18 00:44:29 +0100

    Fix img dir creation

--- chatta_tesoro_adminbot.py
+++ chatta_tesoro_adminbot.py
@@ -133,7 +133,7 @@ def handle(msg):
 
     # Got riddle help image
     elif content_type == 'photo' and USER_STATE[chat_id] == 4:
-        if os.path.isfile('img'):
+        if not os.path.isfile('img'):
             os.mkdir('img')
 
         # Store img
